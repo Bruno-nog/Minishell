49ecc34b385415ac500c0e9e42a36b6d13e707c1
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-06-11 12:57:31 -0300

    backup 11/9

--- srcs/built-in/cd.c
+++ srcs/built-in/cd.c
@@ -6,7 +6,7 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 12:45:10 by brunogue          #+#    #+#             */
-/*   Updated: 2025/06/09 18:58:22 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/06/10 16:13:31 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,35 @@
 
 int	ft_cd(char **args, t_env *env)
 {
-	int		i;
 	char	cwd[PATH_MAX];
-	char	*home_path = NULL;
+	char	*home_path;
 
 	home_path = get_env_value(env, "HOME");
-	ft_printf("%s", home_path);
-	if (!home_path)
-		write (2, "cd: HOME not set\n", 17);
-	i = 1;
-	if (!args[i] || args[i][0] == '\0')
+	if (!args[1] || args[1][0] == '\0')
 	{
-		chdir(home_path);
+		if (!home_path)
+		{
+			ft_putendl_fd("cd: HOME not set.", 2);
+			return (1);
+		}
+		if (chdir(home_path) != 0)
+		{
+			perror("cd");
+			return (1);
+		}
 		getcwd(cwd, PATH_MAX);
+		return (0);
+	}
+	if (args[1] && args[2])
+	{
+		ft_putendl_fd("cd: Too many arguments.\n", 2);
 		return (1);
 	}
-	return (-1);
+	if (chdir(args[1]) != 0)
+	{
+		perror("cd");
+		return (1);
+	}
+	getcwd(cwd, PATH_MAX);
+	return (1);
 }
--- srcs/token.c
+++ srcs/token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 15:28:11 by brunogue          #+#    #+#             */
-/*   Updated: 2025/06/08 16:21:24 by marvin           ###   ########.fr       */
+/*   Updated: 2025/06/10 17:07:12 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ t_token_type	find_token_type(char *str)
 		return (TOKEN_WORD);
 }
 
-t_token *tokenization(t_token *token, char *input)
+t_token	*tokenization(t_token *token, char *input)
 {
 	t_token	*current;
 	char	*value;
