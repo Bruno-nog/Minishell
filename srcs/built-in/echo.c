3585ba1e76a25db7921e2cb331dda5c9af62f750
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-06-24 19:29:23 -0300

    updating ambient var

--- srcs/built-in/echo.c
+++ srcs/built-in/echo.c
@@ -6,15 +6,33 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 17:48:17 by brunogue          #+#    #+#             */
-/*   Updated: 2025/06/23 15:12:58 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/06/24 18:23:32 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *ambient_var(t_env *env)
+void ambient_var(char *arg, t_env *env, int has_next)
 {
-	
+    char *var;
+
+	if (arg[0] == '$' && arg[1] == '$' && arg[2] == '\0')
+		ft_putnbr_fd(getpid(), 1);
+    if (arg[0] == '$')
+    {
+        if (!arg[1] || arg[1] == ' ')
+            ft_putstr_fd("$", 1);
+        else
+        {
+            var = get_env_value(env, arg + 1);
+            if (var)
+                ft_putstr_fd(var, 1);
+        }
+    }
+    else
+        ft_putstr_fd(arg, 1);
+    if (has_next)
+        ft_putstr_fd(" ", 1);
 }
 
 int ft_echo(char **args)
@@ -29,11 +47,7 @@ int ft_echo(char **args)
     }
     while (args[i])
     {
-        if (args[i][0] == '$')
-            ambient_var(args[i], recreate_env(get_shell()->env));
-        ft_putstr_fd(args[i], 1);
-        if (args[i + 1])
-            ft_putstr_fd(" ", 1);
+        ambient_var(args[i], get_shell()->env, args[i + 1] != NULL);
         i++;
     }
     if (!n_flag)
